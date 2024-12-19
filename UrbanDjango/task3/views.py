f0044d0d74fff4f00a1bab036e365e490b1da591
MaxHaunter777 <maxhaunter@gmail.com>
2024-12-19 17:47:11 +0300

    изменил - шаблоны сохраняются в templates/

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class template_cart(TemplateView):
-    template_name = 'cart.html'
+    template_name = 'third_task/cart.html'
 
 class template_games(TemplateView):
-    template_name = 'games.html'
+    template_name = 'third_task/games.html'
 
 class template_platform(TemplateView):
-    template_name = 'platform.html'
+    template_name = 'third_task/platform.html'
